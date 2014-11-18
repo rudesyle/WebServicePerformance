8fb8b2c114cf90cf0461e68c5221381102274037
Rudesyle <rudesyle@gmail.com>
2014-11-18 14:51:00 -0500

    Initial Commit

---
+++ MVCWebApi/Controllers/WebApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PerformanceClass;
+
+namespace MVCWebApi.Controllers
+{
+    public class WebApiController : ApiController
+    {
+        // GET api/WebApi
+        public HttpResponseMessage Get()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new ObjectContent<Person>(new Person(),
+                            GlobalConfiguration.Configuration.Formatters.JsonFormatter);
+            return response;
+        }
+    }
+}
---
+++ WebApi/PerformanceController.cs
@@ -0,0 +1,35 @@
+﻿using PerformanceClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace WebApi
+{
+    public class PerformanceController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage() { Content = new StringContent("Hello World. Time is: " + DateTime.Now.ToString(), Encoding.UTF8, "text/plain") };
+        }
+
+        [HttpGet]
+        public Person HelloWorldJson()
+        {
+            return new Person();
+        }
+
+        [HttpGet]
+        public HttpResponseMessage HelloWorldJson2()
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new ObjectContent<Person>(new Person(),
+                            GlobalConfiguration.Configuration.Formatters.JsonFormatter);
+            return response;
+        }
+    }
+}
